af731c15f8b18861d3a8263aacc711cdbafcccdc
Jayasri P <jayaporalla921@gmail.com>
2026-03-02 17:10:57 +0530

    Created LinkedList Node class

--- LinkedList/LinkedListOperations.java
+++ LinkedList/LinkedListOperations.java
@@ -1,7 +1,7 @@
 package LinkedList;
 
 public class LinkedListOperations {
-    public class Node {
+    public static class Node {
         int data;
         Node next;
 
@@ -10,6 +10,8 @@ public class LinkedListOperations {
         }
     }
     public static void main(String[] args) {
-        
+        Node data1 = new Node(5);
+        Node data2 = new Node(4);
+        Node data3 = new Node(2);
     }
 }
